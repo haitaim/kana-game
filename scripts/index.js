7929c91dbc79ab6e248e9020ed9996cdb8522809
haitaim <76502924+haitaim@users.noreply.github.com>
2022-03-14 11:02:08 -0600

    Remove special character test
    All special characters begin with a capital letter
    Only lowercase input is allowed, so this isn't required

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,10 @@
 const inputDisplay = document.getElementById("key-input");
 document.addEventListener("keydown", (event) => {
-    // special refers to anything not representable with a single character
-    // e.g. tab key is "Tab", so special
-    const keyIsSpecial = event.key.length !== 1;
+    // Check for valid input
     const keyCode = event.key.codePointAt(0);
     const isLowerAlpha = keyCode > 0x60 && keyCode < 0x7b;
     const hasModifier = event.shiftKey || event.ctrlKey || event.metaKey;
-    if (!keyIsSpecial && isLowerAlpha && !hasModifier && !event.repeat) {
+    if (isLowerAlpha && !hasModifier && !event.repeat) {
         console.log(`New key: ${event.key}`);
         inputDisplay.innerText = event.key;
     }
