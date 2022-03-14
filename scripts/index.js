317b448ddd33ddcf0343858e2fef83ca1d86dc65
haitaim <76502924+haitaim@users.noreply.github.com>
2022-03-13 18:02:06 -0600

    Filter key inputs
    Only alpha chars can be input
    Only shift, ctrl, and meta key comboes are filtered
    Windows and alt keys need to be done a different way

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,13 @@
 const inputDisplay = document.getElementById("key-input");
 document.addEventListener("keydown", (event) => {
-    inputDisplay.innerText = event.key;
+    // special refers to anything not representable with a single character
+    // e.g. tab key is "Tab", so special
+    const keyIsSpecial = event.key.length !== 1;
+    const keyCode = event.key.codePointAt(0);
+    const isLowerAlpha = keyCode > 0x60 && keyCode < 0x7b;
+    const hasModifier = event.shiftKey || event.ctrlKey || event.metaKey;
+    if (!keyIsSpecial && isLowerAlpha && !hasModifier && !event.repeat) {
+        console.log(`New key: ${event.key}`);
+        inputDisplay.innerText = event.key;
+    }
 });
